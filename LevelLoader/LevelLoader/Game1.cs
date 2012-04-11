d4cf5e23bbb0099f3efc5f294b5f0da96ef97ed0
Joakim Persson <sir.hoobler@gmail.com>
2012-04-11 15:58:35 +0200

    Second commit

---
+++ LevelLoader/LevelLoader/Game1.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace LevelLoader
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+
+
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        LevelLoader levelLoader;
+        //GameWindow window;
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+
+            Window.AllowUserResizing = false;
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            levelLoader = new LevelLoader("./Levels/level1.xml", this.Content);
+          
+
+            //levelLoader.AddTileTexture(Content.Load<Texture2D>("blue"));
+            levelLoader.AddTileTexture(Content.Load<Texture2D>("red"));
+            levelLoader.AddTileTexture(Content.Load<Texture2D>("green"));
+
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // TODO: Add your update logic here
+
+            levelLoader.MoveCamera(0.5F);
+
+            //if (Keyboard.GetState().IsKeyDown(Keys.A))
+            //    levelLoader.MoveCamera(10);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            levelLoader.Draw(spriteBatch);
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ LevelLoader/LevelLoader/TileTexture.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LevelLoader
+{
+    class TileTexture
+    {
+        private char symbol;
+        private Texture2D texture;
+
+        public TileTexture()
+        {
+        }
+
+        public TileTexture(Texture2D texture, char symbol)
+        {
+            this.texture = texture;
+            this.symbol = symbol;
+        }
+
+        public Texture2D Texture
+        {
+            get { return texture; }
+            set { texture = value; }
+        }
+
+        public char Symbol
+        {
+            get { return symbol; }
+            set { symbol = value; }
+        }
+
+        public Texture2D GetTexture(char symbol)
+        {
+            if (this.symbol == symbol)
+            {
+                return texture;
+            }
+            else
+            {
+                return null;
+            }
+        }
+ 
+    }
+}
